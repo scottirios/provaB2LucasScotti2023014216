8ed1e1264cf51baf193b999002e42db2e97a47ec
Lucas Scotti <lucas_scotti.13@hotmail.com>
2025-07-01 20:57:12 -0300

    feat: Criando Find-All-Books-Service

---
+++ src/books/find-all-books.service.ts
@@ -0,0 +1,47 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { BooksRepository } from './books-repository';
+
+export interface Book {
+  id: string;
+  title: string;
+  author: string;
+  publicationYear: number;
+  isbn: string;
+  createdAt: string | Date | undefined;
+  updatedAt: string | Date | null | undefined;
+}
+
+type FindAllBooksServiceResponse = {
+  books: Book[];
+};
+
+@Injectable()
+export class FetchRecentModelsService {
+  constructor(private booksRepository: BooksRepository) {}
+
+  async execute(): Promise<FindAllBooksServiceResponse> {
+    const books = await this.booksRepository.findAll();
+
+    const newBooks: Book[] = [];
+
+    if (!books) {
+      throw new HttpException('Books not found.', HttpStatus.NOT_FOUND);
+    }
+
+    for (const book of books) {
+      newBooks.push({
+        id: book.id?.toString() || '',
+        title: book.title,
+        author: book.author,
+        publicationYear: book.publicationYear,
+        isbn: book.isbn,
+        createdAt: book.createdAt ? new Date(book.createdAt) : undefined,
+        updatedAt: book.updatedAt ? new Date(book.updatedAt) : undefined,
+      });
+    }
+
+    return {
+      books: newBooks,
+    };
+  }
+}
