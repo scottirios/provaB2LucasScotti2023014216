77d9c10b74224ffbc7ff424e5c8fb2b8e43a2bd6
Lucas Scotti <lucas_scotti.13@hotmail.com>
2025-07-01 20:47:16 -0300

    feat: Criando Create-Book-Service

---
+++ src/books/create-book-service.ts
@@ -0,0 +1,35 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { BooksRepository } from './books-repository';
+
+interface CreateBookServiceRequest {
+  title: string;
+  author: string;
+  publicationYear: number;
+  isbn: string;
+}
+
+@Injectable()
+export class CreateBookService {
+  constructor(private booksRepository: BooksRepository) {}
+
+  async execute({
+    title,
+    author,
+    publicationYear,
+    isbn,
+  }: CreateBookServiceRequest): Promise<void> {
+    const TitleWithSameName = await this.booksRepository.findByTitle(title);
+
+    if (TitleWithSameName) {
+      throw new ConflictException('Book with same title already exists.');
+    }
+
+    const isbnAlreadyExists = await this.booksRepository.findByISBN(isbn);
+
+    if (isbnAlreadyExists) {
+      throw new ConflictException('Book with same ISBN already exists.');
+    }
+
+    await this.booksRepository.create({ title, author, publicationYear, isbn });
+  }
+}
